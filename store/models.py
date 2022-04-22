0fdcc38d6b5a2e4e3c3f992c99dc6f0506dc88b6
Erica Siegel <siegel.erica@gmail.com>
2022-04-22 14:07:01 -0700

    created models for store app

--- store/models.py
+++ store/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -10,6 +17,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now_add=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
     
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
